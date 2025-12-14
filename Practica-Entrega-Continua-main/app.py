4a93dd308ad88a4a455c12dcc23e0435a7f07d3c
leslief26 <74542531+leslief26@users.noreply.github.com>
2025-12-14 15:09:27 -0400

    Add files via upload

---
+++ Practica-Entrega-Continua-main/app.py
@@ -0,0 +1,18 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return '''
+    <html>
+        <head><title>Hola Mundo</title></head>
+        <body style="text-align: center; padding-top: 50px; font-family: Arial;">
+            <h1>¡Hola Mundo desde Docker! 🐳</h1>
+            <p>Esta es mi aplicación DevOps</p>
+        </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
